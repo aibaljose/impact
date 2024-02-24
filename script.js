0b97aa2a53dc6925dec40f6d406b79e32dd4c4e1
aibaljose <aibaljosej@gmail.com>
2024-02-24 11:19:25 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById("studyjam").addEventListener('click', () => {
     window.open("https://chat.whatsapp.com/DmcjBNA5ojOEwVULUxhGFV", "_blank");
 });
+document.getElementById("appd").addEventListener('click', () => {
+    window.open("https://chat.whatsapp.com/I9U7eBjy9La853vcIQLyNN", "_blank");
+});
+
+
